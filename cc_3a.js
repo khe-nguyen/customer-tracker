b2a6d0f0b354b4351c5ccbbe29efcf589227b62d
Khe Nguyen <khenguyen@usf.edu>
2025-09-14 22:05:37 -0400

    I created an array "customer" with 4 products.

---
+++ cc_3a.js
@@ -0,0 +1,10 @@
+// Coding Challenge 3a - Sept 14
+
+// 2. Create Customer Records
+let customers = [
+    {name: "Tomas Godoy Pastore", email: "tomaspastore@gmail.com", purchase: "laptop" },
+    {name: "Maddy Kanda", email: "madeleinekanda@gmail.com", purchase: "wine"},
+    {name: "Augusto Trufelman", email: "augustotrufelman@gmail.com", purchase: "shoes"},
+    {name: "Kelly Khe Nguyen", email: "kellynguyen@gmail.com", purchase: "comforter"},
+    ];
+
