f560f3d0adbfa772a4000ea4bcafefcf547efe24
Khe Nguyen <khenguyen@usf.edu>
2025-09-14 23:05:31 -0400

    fixed format and display customer info

--- cc_3a.js
+++ cc_3a.js
@@ -2,30 +2,32 @@
 
 // 2. Create Customer Records
 let customers = [
-    {name: "Maddy Kanda", email: "madeleinekanda@gmail.com", purchase: "wine"},
-    {name: "Tomas Godoy Pastore", email: "tomaspastore@gmail.com", purchase: "laptop" },
-    {name: "Augusto Trufelman", email: "augustotrufelman@gmail.com", purchase: "shoes"},
-    ];
-
-
+    {name: "Maddy Kanda", email: "madeleinekanda@gmail.com", purchase: ["wine"]},
+    {name: "Tomas Godoy Pastore", email: "tomaspastore@gmail.com", purchase: ["laptop"]},
+    {name: "Augusto Trufelman", email: "augustotrufelman@gmail.com", purchase: ["shoes"]},
+];
 
 // 3. Add and Remove Data
-// Add a new customer
-customers.push({name: "Kelly Khe Nguyen", email: "kellynguyen@gmail.com", purchase: "comforter"});
-
-// Remove the first customer
+customers.push({name: "Kelly Khe Nguyen", email: "kellynguyen@gmail.com", purchase: ["comforter"]});
 customers.shift();
-console.log(customers)
 
 // 4. Update Customer Info
+console.log(customers[0].email = "tomasnewmail@gmail.com");
 
-// Update the email of one existing customer
-console.log(customers[1].email = "tomasnewmail@gmail.com");
-customers.forEach(obj => console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchase}`));
+customers.forEach(customer => {
+console.log(`${customer.name} | ${customer.email} | ${customer.purchase}`);
+});
 
-// Add a new purchase to another customer
-console.log(customers[2].purchase = "jacket");
-customers.forEach(obj => console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchase}`));
+console.log(customers[2].purchase.push("jacket"));
 
+customers.forEach(customer => {
+console.log(`${customer.name} | ${customer.email} | ${customer.purchase}`);
+});
 
-
+// 5. Display Customer Information
+customers.forEach(customer => {
+    console.log(`Name: ${customer.name}`);
+    console.log(`Email: ${customer.email}`);
+    console.log(`Total Purchases: ${customer.purchase.length}`);
+    console.log("---------------------------");
+});
