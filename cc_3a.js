5902b8f6978445f463537f5a70c3ffae59fba643
Khe Nguyen <khenguyen@usf.edu>
2025-09-14 22:37:57 -0400

    update customer info

--- cc_3a.js
+++ cc_3a.js
@@ -15,4 +15,17 @@ customers.push({name: "Kelly Khe Nguyen", email: "kellynguyen@gmail.com", purcha
 
 // Remove the first customer
 customers.shift();
+console.log(customers)
+
+// 4. Update Customer Info
+
+// Update the email of one existing customer
+console.log(customers[1].email = "tomasnewmail@gmail.com");
+customers.forEach(obj => console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchase}`));
+
+// Add a new purchase to another customer
+console.log(customers[2].purchase = "jacket");
+customers.forEach(obj => console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchase}`));
+
+
 
